c2980ecc0b84425a801db186e5598d45d3e0d338
VictorFlores99 <a23310377@live.ceti.mx>
2024-11-01 01:42:46 +0000

    clase 4/Examen

--- src/test.cpp
+++ src/test.cpp
@@ -15,47 +15,82 @@ int main(int argc, char const *argv[])
     int ancho = 100;
     int altura = 50;
     int tiempo = 0;
+    int brazosP1X1=200;
+    int brazosP1Y1=60;
+    int brazosP1Y2=70;
+    int brazosP2X1=85;
+    int brazosP2Y1 = 60;
+    int brazosP2Y2 = 70;
+
+
 
     while(true)
     {
-            
-        auto can = Canvas(300, 200);      // Canvas es un objeto que se construye con ancho y alto
+
+        auto can = Canvas(300, 200); // Canvas es un objeto que se construye con ancho y alto
         can.DrawPointCircle(75, 50, 10);
         can.DrawPointLine(75, 60, 75, 80);
         can.DrawPointLine(75, 80, 85, 100);
         can.DrawPointLine(75, 80, 65, 100);
-        can.DrawPointLine(75, 70, 85, 60);
-        can.DrawPointLine(75, 70, 85, 70);
-        can.DrawPointCircle(ancho,altura,10);
-        can.DrawPointCircle(200, 50, 10);
-        can.DrawPointLine(200, 60, 200, 80);
-        can.DrawPointLine(200, 80, 210, 100);
-        can.DrawPointLine(200, 80, 190, 100);
-        can.DrawPointLine(200, 70, 190, 60);
-        can.DrawPointLine(200, 70, 190, 70);
-        Screen pantalla = Screen::Create(Dimension::Full(), Dimension::Full());  //Los naranjas son objetos
+        can.DrawPointLine(75, 70, brazosP2X1, brazosP2Y1);
+        can.DrawPointLine(75, 70, brazosP2X1, brazosP2Y2);
+        can.DrawPointCircle(ancho, altura, 10);
+        can.DrawPointCircle(210, 50, 10);
+        can.DrawPointLine(210, 60, 210, 80);
+        can.DrawPointLine(210, 80, 220, 100);
+        can.DrawPointLine(210, 80, 200, 100);
+        can.DrawPointLine(210, 70, brazosP1X1, brazosP1Y1);
+        can.DrawPointLine(210, 70, brazosP1X1, brazosP1Y2);
+        Screen pantalla = Screen::Create(Dimension::Full(), Dimension::Full()); // Los naranjas son objetos
         Element lienzo = bgcolor(Color::Blue, canvas(&can));
         Render(pantalla, lienzo);
         std::cout << reset_position;
         pantalla.Print();
         reset_position = pantalla.ResetPosition(true);
-        this_thread::sleep_for(0.1s); //Delay entre los ciclos. FPS
-        ancho++;
+        this_thread::sleep_for(0.01s); // Delay entre los ciclos. FPS
+
         tiempo++;
-        frame++; //Numero de frame en el tiempo
+        frame++; // Numero de frame en el tiempo
 
-        if(tiempo>0 && tiempo <41){
+        if (tiempo > 0 && tiempo < 41)
+        {
             altura--;
-        }if(tiempo>41 && tiempo <80){
+            ancho++;
+            brazosP2X1 = 95;
+            brazosP2Y1 = 60;
+            brazosP2Y2 = 70;
+        }
+        if (tiempo > 41 && tiempo < 80)
+        {
             altura++;
+            ancho++;
+            brazosP2X1 = 85;
+            brazosP2Y1 = 60;
+            brazosP2Y2 = 70;
+        }
+        if (tiempo > 80 && tiempo < 119)
+        {
+            altura--;
+            ancho--;
+            brazosP1X1 = 190;
+            brazosP1Y1 = 50;
+            brazosP1Y2 = 60;
+        }
+        if (tiempo > 119 && tiempo < 158)
+        {
+            altura++;
+            ancho--;
+            brazosP1X1 = 200;
+            brazosP1Y1 = 60;
+            brazosP1Y2 = 70;
         }
 
-        
-
-        if(tiempo==80){
-            return 0;
+        if (tiempo == 158)
+        {
+            tiempo = 0;
+            altura=50;
+            ancho=100;
         }
-        
     }
 
     return 0;
